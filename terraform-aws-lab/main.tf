9f4458ab89e02ac8c40ef68a9ac544a68b8924a3
kamal <kkamalhere@gmail.com>
2026-08-30 13:43:05 -0400

    Configure S3 backend for Terraform state

--- terraform-aws-lab/main.tf
+++ terraform-aws-lab/main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+    backend "s3" {
+    bucket = "kkama-terraform-state-2026-001"
+    key    = "terraform-aws-lab/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
